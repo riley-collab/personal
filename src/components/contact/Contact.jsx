76ecb6c77aea84be26392fa2d2d7a700e8f591ad
riley-collab <riley.morris@mail.utoronto.ca>
2024-11-14 14:01:31 -0500

    Updated with Sanofi exp and skils

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -44,7 +44,7 @@ const Contact = () => {
           <article className='contact__option'>
             <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
-            <h5>riley.morris@mail.utoronto.ca</h5>
+            <h5>riley.morris@alumni.utoronto.ca</h5>
             <a href="mailto:riley.morris@mail.utoronto.ca" target="_blank">Send a message</a>
           </article>
           {/* <article className='contact__option'>
